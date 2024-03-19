e87fd2d3f879a90ef0f1300a688152bab8909985
TimonAME <timon-a@outlook.at>
2024-03-19 16:32:54 +0100

    layout prelobby

--- frontend/pages/prelobby/index.vue
+++ frontend/pages/prelobby/index.vue
@@ -3,21 +3,21 @@
         <!-- dotted background from: https://bg.ibelick.com/ -->
         <!-- Punkte im Hintergrund: bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] -->
         <div
-            class="flex flex-col justify-start h-screen overflow-hidden sm:mx-0 mx-4"
+            class="flex flex-col justify-between h-screen overflow-hidden sm:mx-0 mx-4"
         >
             <Navbar class="z-30" />
             <Lobbyname class="z-30" />
 
             <div
-                class="relative z-10 flex flex-col justify-center items-center mt-5"
+                class="flex sm:flex-row flex-col justify-center items-center w-full lg:gap-32 md:gap-24 sm:gap-16 gap-4"
             >
                 <UserDisplay />
+                <div>
+                    <StartGame />
+                    <InviteInfo />
+                </div>
             </div>
-            <div
-                class="relative z-10 flex flex-col justify-center items-center mt-5"
-            >
-                <StartGame />
-            </div>
+            <div></div>
         </div>
     </div>
 </template>
@@ -27,6 +27,7 @@ import Navbar from "~/components/prelobby/Navbar.vue";
 import Lobbyname from "~/components/prelobby/Lobbyname.vue";
 import StartGame from "~/components/prelobby/StartGame.vue";
 import UserDisplay from "~/components/prelobby/UserDisplay.vue";
+import InviteInfo from "~/components/prelobby/InviteInfo.vue";
 </script>
 
 <style lang="scss" scoped></style>
